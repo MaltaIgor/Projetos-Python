0c0db9b9e9a71b6590a38dab50de597c714b6cc5
Igor Malta <igorbrunomalta@gmail.com>
2022-01-11 20:42:21 -0300

    Update jogadores_futebol.py

--- CV_Projects/jogadores_futebol.py
+++ CV_Projects/jogadores_futebol.py
@@ -5,21 +5,27 @@ while True:
     jogador.clear()
     jogador['nome'] = str(input('Nome do Jogador: '))
     tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
     for c in range (0,tot):
-        partidas.append(int(input(f'    Quantos gols na partida {c+1}')))
+        partidas.append(int(input(f'    Quantos gols na partida {c+1}: ')))
     jogador['gols'] = partidas [:]
     jogador['total'] = sum(partidas)
     time.append(jogador.copy())
-    resp = str(input('Deseja continuar? [S/N]')).upper()[0]
+    resp = str(input('Deseja continuar? [S/N] ')).upper()[0]
     if resp == 'N':
         break
-    print('-='*30)
-    print(jogador)
-    print('-='*30)
-    for k, v in jogador.items():
-        print(f'O campo {k} tem o valor {v}')
-    print('-='*30)
-    print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
-    for i, v in enumerate(jogador['gols']):
-        print(f'    => Na partida {i+1}, fez {v} gols.')
-    print(f'Foi um total de {jogador["total"]} gols.')
+
+print('-='*30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40) 
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)    
+
+while True:
