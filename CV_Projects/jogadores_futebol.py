235449a77df04c2ef9591ba714c7974b3730165f
Igor Malta <igorbrunomalta@gmail.com>
2022-01-11 23:12:55 -0300

    Update jogadores_futebol.py

--- CV_Projects/jogadores_futebol.py
+++ CV_Projects/jogadores_futebol.py
@@ -29,3 +29,14 @@ for k, v in enumerate(time):
 print('-' * 40)    
 
 while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe um jogador com código {busca}!')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}: ')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols. ')
+    print('-' * 40) 
+print('Finalizando')
