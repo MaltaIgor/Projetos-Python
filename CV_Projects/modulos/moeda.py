26ee72e356a911d41572e783b5e9ca066c8ee471
Igor Malta <igorbrunomalta@gmail.com>
2022-01-19 08:26:36 -0300

    updating

--- CV_Projects/modulos/moeda.py
+++ CV_Projects/modulos/moeda.py
@@ -20,3 +20,15 @@ def metade(preço, format = False):
 
 def moeda(preço = 0, moeda = 'R$'):
     return f'{moeda}{preço:8.2f}'.replace('.',',')
+
+
+def resumo(preço=0,aum=10,dim=10):
+    print(f'-'* 30)
+    print('RESUMO DO VALOR'.center(30))
+    print(f'-'* 30)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'Aumentando o preço: \t{aumentar(preço, aum, True)}')
+    print(f'Diminuindo o preço: \t{diminuir(preço, dim, True)}')
+    print(f'-'* 30)
