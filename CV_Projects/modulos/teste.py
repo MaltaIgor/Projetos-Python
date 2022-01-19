fb1efd209c605e70ec79ac3ca29d7ed8d196d758
Igor Malta <igorbrunomalta@gmail.com>
2022-01-19 07:57:21 -0300

    updating

---
+++ CV_Projects/modulos/teste.py
@@ -0,0 +1,6 @@
+import moeda
+
+p = float(input('digite o preço: R$'))
+print(f'A metade de R${p} é R${moeda.metade(p)}')
+print(f'O dobro de R${p} é R${moeda.dobro(p)}')
+print(f'Aumentando em 10% o valor é R${moeda.aumentar(p,10)}')
