8d3dc41ab1fed44fe2afa36176a8c0029bd7504b
Igor Malta <igorbrunomalta@gmail.com>
2022-01-19 08:44:11 -0300

    updating

--- CV_Projects/modulos/teste.py
+++ CV_Projects/modulos/teste.py
@@ -1,6 +1,9 @@
-from pacote import moeda
+from pacote import moeda, dado
 
-p = float(input('digite o preço: R$'))
+p = dado.leiadinheiro('digite o preço: R$')
+
+
+
 print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p,True)}')
 print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
 print(f'Aumentando em 10% o valor é {moeda.aumentar(p,10, True)}')
