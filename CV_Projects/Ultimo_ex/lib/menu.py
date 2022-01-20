08556de2e442f2f4f17426cb00a7a43acb13e357
Igor Malta <igorbrunomalta@gmail.com>
2022-01-20 09:01:30 -0300

    update

--- CV_Projects/Ultimo_ex/lib/menu.py
+++ CV_Projects/Ultimo_ex/lib/menu.py
@@ -1,5 +1,14 @@
 
 from interface import *
 
-
-resposta = menu(['Criar arquivo','Cadastrar pessoas','Listar pessoas', 'Sair do Sistema'])
+while True:
+    resposta = menu(['Ver Pessoas Cadastradas','Cadastrar Nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        print('Opção 1')
+    elif resposta == 2:
+        print('Opção 2')
+    elif resposta == 3:
+        print('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO: Digite uma opção válida.\033[m' )
